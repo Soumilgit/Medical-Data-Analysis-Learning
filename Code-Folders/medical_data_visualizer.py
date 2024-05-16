f4b91f0038cb1b17500e092765d41ba930856a75
Soumil Mukhopadhyay <soumil.m@somaiya.edu>
2024-05-16 20:39:06 +0530

    Update medical_data_visualizer.py

--- Code-Folders/medical_data_visualizer.py
+++ Code-Folders/medical_data_visualizer.py
@@ -14,7 +14,43 @@ df['gluc'] = (df['gluc'] > 1).astype(int)
 
 df['alcohol'] = (df['alco'] == 'yes').astype(int)
 
+def drw_cat_plt():
+    
+    datf_cat = pd.melt(datf, id_vars=['cardio'],
+                     value_vars=['cholesterol', 'smoke', 'alcohol', 'obese'])
 
+    
+    datf_cat = datf_cat.groupby(['cardio', 'variable',]).size().reset_index()
+    datf_cat = datf_cat.rename(columns={0: 'total'})
+
+    
+    graph = sns.catplot(data=datf_cat, kind="bar", x="variable", y="total", hue="value", col="cardio")
+    fig = graph.fig
+
+    
+    fig.savefig('catplot.png')
+    return fig
+
+def drw_ht_mp():
+   
+   
+
+    
+    corr = datf_heat.corr()
+
+    
+    mask = np.triu(np.ones_like(corr, dtype=bool))
+
+    
+    fig, ax = plt.subplots(figsize=(25,18))
+
+    
+    sns.heatmap(corr, mask=mask, square=True, linewidths=1.3, annot=True)
+
+    
+    fig.savefig('heatmap.png')
+    return fig
+    
 df_clean = df[
     (df['ap_lo'] <= df['ap_hi']) &
     (df['height'] >= df['height'].quantile(0.025)) &
